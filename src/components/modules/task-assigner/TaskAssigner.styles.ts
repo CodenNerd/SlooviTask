2214140837ccd82b83d9f3f6ce9f767945eb63e8
AbdulAzeez Atanda <aatanda.dammy@gmail.com>
2022-06-27 04:11:08 +0100

    change fields outline color

--- src/components/modules/task-assigner/TaskAssigner.styles.ts
+++ src/components/modules/task-assigner/TaskAssigner.styles.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export default styled.div`
-    width: 400px;
+    max-width: 400px;
     border: 1px solid lightgrey;
     border-radius: .2rem;
-    margin: 50px;
+    margin: 50px auto;
 `
--- src/utils/style-enums.ts
+++ src/utils/style-enums.ts
@@ -1,6 +1,6 @@
 export const border = '1px solid #ccc';
 export const formBackgroundColor = '#c5e3f666';
-export const fieldsOutline = '1px solid lightblue';
+export const fieldsOutline = '2px solid #bfdbf6';
 export const fieldsBackground = 'white';
 export const fieldsBorder = '1px solid #ccc';
 export const fieldsBorderRadius = '3px';
